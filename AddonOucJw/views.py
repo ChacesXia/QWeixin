6db3a87b0ecbc19040704085e93b010387b4f251
DoubleL <admin@it592.com>
2016-07-03 16:37:15 +0800

    当未绑定时，转到绑定界面

--- AddonOucJw/views.py
+++ AddonOucJw/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext, loader
